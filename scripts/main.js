f1c553d5adbbd697cb4aff1cdffb7543df94f527
eromnoj <j.moreschi@outlook.fr>
2022-06-05 20:51:20 +0200

    Select good time zone for forecast at midday

--- scripts/main.js
+++ scripts/main.js
@@ -42,17 +42,16 @@ const history = (parent, value) => { //fonction pour constituer un historique de
   let checkTable = []
   Array.from(parent.children).forEach(child => { // on regarde si la ville ne s'y trouve pas déjà /!\ .children renvoie une nodeList qu'il faut convertir en tableau, d'où l'utilisation de Array.from
 
-    console.log(child.value + " " + value) // en parcourant l'ensemble des enfants de select
+   // en parcourant l'ensemble des enfants de select
     if (child.value === value) { //si la value d'une des options est identique à l'ID recherché on rempli le tableau
       checkTable.push(true)
-      console.log(checkTable)
 
     }
   })
   if (checkTable.length === 0) { //si le tableau est, ce qui veut dire que la ville ne s'y trouve pas déjà on crée une nouvelle option qu'on ajoute à la suite
     let option = document.createElement('option')
     option.value = value
-    console.log(currentWeather.city)
+   
     option.textContent = currentWeather.city
     parent.appendChild(option)
   }
@@ -69,21 +68,21 @@ const getData = (cityId = '2987914') => { //fonction qui récupère les données
         icon: data.list[0].weather[0].icon
       }
       for (let i = 0; i < data.list.length; i++) {// boucle pour les prévisions
-        if (data.list[i].dt % 86400 === 43200) { // l'API renvoie une prévision pour toutes les 3 heures, on fait un modulo pour ne recupérerer qu'une prévision toutes les 24h 
-          forecast.push({                        // se base uniquement sur le timestamp donc l'heure de la prévision sera toujours à midi heure française quelque soit le pays
-            date: dayjs(data.list[i].dt_txt).locale('fr').format('D MMM YYYY'),
+        console.log(dayjs((data.list[i].dt + data.city.timezone) * 1000).format('HH:mm'))
+        if (dayjs((data.list[i].dt + data.city.timezone) * 1000).format('HH') === '11' || dayjs((data.list[i].dt + data.city.timezone) * 1000).format('HH') === '12' || dayjs((data.list[i].dt + data.city.timezone) * 1000).format('HH') === '13') { // l'API renvoie une prévision pour toutes les 3 heures sur 5 jours 
+          forecast.push({              // On prend en compte l'amplitude horaire de 3h, et on choisit la prévision à la mi-journée en fonction du fuseau horaire          ^       ^-------------------------------------^           ^    
+            date: dayjs((data.list[i].dt + data.city.timezone) * 1000).locale('fr').format('ddd D MMM YYYY'),                                                  //Appel librairie    TStamp(unix) du serveur+TZ*conversion    formatage en Heure 
             temperature: formatTemp(data.list[i].main.temp),
             weather: data.list[i].weather[0].description,
             icon: data.list[i].weather[0].icon
           })
         }
       }
-
       history(city, cityId)
       createForcast()
     })
     .catch((err) => { //on affiche une alerte si il y a eu un problème dans la requête
-      alert('Mauvais identifiant, mauvais ID, ou serveur injoignable ' + err)
+      alert('Mauvais identifiant, mauvais APPID, ou serveur injoignable ' + err)
     })
 }
 
